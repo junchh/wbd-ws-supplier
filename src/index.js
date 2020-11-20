cff93a9dcdaef28e5499043eba47eb85ff52ca33
junchh <cjunho9868@gmail.com>
2020-11-20 18:58:08 +0700

    local mysql connection setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 const express = require('express')
+const mysql = require('mysql2/promise')
 const app = express()
 
+let connection = null
 
-app.get('', (req, res) => {
-    res.send("hello")
+app.get('', async (req, res) => {
+    
+    const [rows, fields] = await connection.execute('SELECT * FROM `ingredients`')
+    console.log(rows)
+    console.log(fields)
+    res.send("success")
 })
 
-app.listen(6900, () => {
+app.listen(6900, async () => {
     console.log('Supplier\'s Web Service is up and running on port 6900.')
+
+    connection = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'ws-supplier',
+        password: '123'
+    });
 })
