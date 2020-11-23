a1a4f8dab26f070f8061fe4a58c44b34edea1dda
Matthew Kevin Amadeus <13518035@std.stei.itb.ac.id>
2020-11-23 09:57:41 +0700

    Added DB_PORT to env

--- src/db/index.js
+++ src/db/index.js
@@ -3,6 +3,7 @@ const mysql = require("mysql2/promise");
 const createMySqlConnection = async () =>
   await mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
     password: process.env.DB_PASS,
